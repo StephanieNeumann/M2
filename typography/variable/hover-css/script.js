57c0622a5389a5e4b97527e7b9698a4086e105a4
StephanieNeumann <35526741+StephanieNeumann@users.noreply.github.com>
2018-10-09 13:49:28 +0200

    Update script.js

--- typography/variable/hover-css/script.js
+++ typography/variable/hover-css/script.js
@@ -1,53 +1,4 @@
 
-
-/*Another code snippet from another example*/
-
-/*
-document.addEventListener('DOMContentLoaded',function(event){
-  // array with texts to type in typewriter
-  var dataText = [ "Amsterdam.", "Full Service.", "Webdevelopment.", "Wij zijn Occhio!"];
-  
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    if (i < (text.length)) {
-      // add next character to h1
-     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-
-      // wait for a while and call this function again for next character
-      setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback)
-      }, 100);
-    }
-    // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
-      // call callback after timeout
-      setTimeout(fnCallback, 700);
-    }
-  }
-  // start a typewriter animation for a text in the dataText array
-   function StartTextAnimation(i) {
-     if (typeof dataText[i] == 'undefined'){
-        setTimeout(function() {
-          StartTextAnimation(0);
-        }, 20000);
-     }
-     // check if dataText[i] exists
-    if (i < dataText[i].length) {
-      // text exists! start typewriter animation
-     typeWriter(dataText[i], 0, function(){
-       // after callback (and whole text has been animated), start next text
-       StartTextAnimation(i + 1);
-     });
-    }
-  }
-  // start the text animation
-  StartTextAnimation(0);
-*/
-
-
-
 /*Animate the rimes as soon as the page is loaded*/
 
 if (document.readyState != 'loading'){
@@ -82,20 +33,21 @@ function onDocumentReady() {
     .getElementById('secondRime')
     .addEventListener('click', onSecondRimePointerOver);
 
+  
+  
   document
-    .getElementById('right')
+    .getElementById('head')
     .addEventListener('pointerover', onHover);
   
     document
-    .getElementById('tight')
+    .getElementById('heart')
     .addEventListener('pointerover', onHover);
+  
+    
 
-  document
-    .getElementById('quite')
-    .addEventListener('pointerover', onHover);  
     
-    
-    
+  
+   
 }
 
 
@@ -104,29 +56,69 @@ function onDocumentReady() {
 function updateFont(el, d) {
 
   let size = parseInt(d);
+
   
-  if (size < 100){
+  if (size < 100){ // cursor close to word
 
     el.style.fontVariationSettings = '\'term\' ' + 100 + ', \'wght\' ' + 200;
+    
+    if(el=head){
+      document.getElementById("head").style.letterSpacing = "0.5px";
+      el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 200;
+    }
+    if(el=heart){
+      document.getElementById("heart").style.letterSpacing = "0.5px";
+      el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 200;
+    }
+    
+    document.getElementById("quite").style.opacity = "1";
+    
 
   } 
-  else if (size > 300){
+  else if (size > 300){ // cursor the furtest away
 
     el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 50 + ', \'inkt\' ' + 0;
-  }
-  else{
 
+    
+    if(el=head){
+      document.getElementById("head").style.letterSpacing = "2px";
+      el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 50;
+    }
+
+    if(el=heart){
+      document.getElementById("heart").style.letterSpacing = "2px";
+      el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 50;
+    }
+    
+
+    document.getElementById("quite").style.opacity = "0.3";
+    
+  }
+  else{ // cursor between 100-300
 
     el.style.fontVariationSettings = '\'term\' ' + 50 + ', \'wght\' ' + 170 + ', \'inkt\' ' + 100;
 
-  }
+    
+    if(el=head){
+      document.getElementById("head").style.letterSpacing = "1px";
+      el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 100;
+    }
 
- 
+    if(el=heart){
+      document.getElementById("heart").style.letterSpacing = "1px";
+      el.style.fontVariationSettings = '\'term\' ' + 0 + ', \'wght\' ' + 100;
+    }
+    
+    document.getElementById("quite").style.opacity = "0.7";
+  } 
+
+  
+
 }
 
 function calcDistance(el, pointerX, pointerY) {
   let rect = el.getBoundingClientRect();
-  let xSpan = rect.left;
+  let xSpan = rect.right;
   let ySpan = rect.top;
 
   let test = pointerX -xSpan;
@@ -143,9 +135,35 @@ function updateElementFonts(ev){
   el = document.getElementById('secondRime');
   updateFont(el, calcDistance(el, pointerX, pointerY)); 
 
+  
+  el = document.getElementById('tight');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  el = document.getElementById('quite');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  el = document.getElementById('right');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  el = document.getElementById('all');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  el = document.getElementById('small');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  el = document.getElementById('head');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  el = document.getElementById('heart');
+  updateFont(el, calcDistance(el, pointerX, pointerY)); 
+  
 }
 
 
+function onHover(el, d){
+  
+  
+  document.getElementById("head").style.letterSpacing = "0.2px";
+  document.getElementById("heart").style.letterSpacing = "0.2px";
+  document.getElementById("head").style.fontVariationSettings = '\'term\' ' + 100 + ', \'wght\' ' + 50;
+  document.getElementById("heart").style.fontVariationSettings = '\'term\' ' + 100 + ', \'wght\' ' + 50;
+
+
+}
 
 
 
@@ -165,15 +183,6 @@ function onSecondRimePointerOver(){
   animateSecond();
   
 }
-
-function onHover(){
-  
-  
-}
-
-
-
-
 
 
 
@@ -227,47 +236,5 @@ function animateSecond(e) {
   // Use this helper function to stop the animation
   //handleStop(el, player);
 
-}
-
-
-/*
-function animateFeedForward(e) {
-
-  // Two keyframes: start and stop
-  const keyframes = [
-    { fontVariationSettings: '\'wdth\' 0, \'wght\' 50' },
-    { fontVariationSettings: '\'wdth\' 100, \'wght\' 200' }
-  ];
-  // Set up some options so anim loops continuously
-  const options = {
-    iterations: 1,
-    delay: 100,
-    direction: 'alternate',
-    duration: 700,
-    easing: 'ease-in-out'
-  };
-
-  //let el = e.target;
-
-  // Animate!
-  let player = getRime.animate(keyframes, options);
-
-  //Use this helper function to stop the animation
-  //handleStop(el, player);
-
-}
-*/
-
-
-// Reusable function to stop animation when 'dubbelclick' happens on an element
-function handleStop(el, player) {
-  el.addEventListener(
-    '',
-    () => {
-      console.log('Stopping animation');
-      player.cancel();
-    },
-    { once: true }
-  );
 }
 
